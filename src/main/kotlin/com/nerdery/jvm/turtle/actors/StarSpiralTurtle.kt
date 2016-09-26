c7ee676e0b9bcd36b6265dae35831630dae50f3a
Josh Klun <jklun@nerdery.com>
2016-09-26 10:14:24 -0500

    Remove unused sample code, now that we have real submissions.

--- src/main/kotlin/com/nerdery/jvm/turtle/actors/StarSpiralTurtle.kt
+++ src/main/kotlin/com/nerdery/jvm/turtle/actors/StarSpiralTurtle.kt
@@ -12,15 +12,12 @@ import java.awt.Color
 
 class StarSpiralTurtle : TurtleActor() {
 
-    var runs = 0
     override fun draw(turtle: Turtle) {
         val iterations = 20
         val sizeInc = 10.0
-        turtle.penUp().forward(runs * iterations * sizeInc).penDown()
         turtle.setPenColor(Color.YELLOW)
         (1 .. iterations).forEach {
             turtle.forward(it * sizeInc).right(144.0)
         }
-        runs++
     }
 }
